4d23ec228e963b74985c1a34a0ca0058a6c17876
skh2000k <skh2000k@gmail.com>
2018-05-14 13:32:44 +0900

    프로젝트생성

---
+++ src/test/java/com/msjump/springwebservice/MsjumpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.msjump.springwebservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsjumpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
