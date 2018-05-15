78244c76bb6ab9cc592b1815283b7b4a86240829
skh2000k <skh2000k@gmail.com>
2018-05-15 18:34:22 +0900

    1

--- src/main/java/com/msjump/webservice/Application.java
+++ src/main/java/com/msjump/webservice/Application.java
@@ -1,12 +1,12 @@
-package com.msjump.webservice;
+package com.msjump.webservice; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
-public class Application { 
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
