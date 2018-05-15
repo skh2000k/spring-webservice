a2ae2d87fede39edc1c71309ca8f5a88b752c6ef
skh2000k <skh2000k@gmail.com>
2018-05-15 17:34:08 +0900

    no message

--- src/main/java/com/msjump/webservice/Application.java
+++ src/main/java/com/msjump/webservice/Application.java
@@ -2,11 +2,13 @@ package com.msjump.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing È°¼ºÈ­
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/com/msjump/webservice/web/WebRestController.java
+++ src/main/java/com/msjump/webservice/web/WebRestController.java
@@ -1,12 +1,34 @@
 package com.msjump.webservice.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.msjump.webservice.PostsService;
+import com.msjump.webservice.domain.posts.PostsRepository;
+import com.msjump.webservice.domain.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+	
+	@Autowired
+    PostsRepository postsRepository;
+	
+    private PostsService postsService;
+	
 	@GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
     }
+	
+	 @PostMapping("/posts")
+	    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+	        return postsService.save(dto);
+	    }
+
 }
